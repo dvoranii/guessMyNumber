a107d304c1cbfbe4db5862a9ae221fdad3460019
dvoranii <dvoranii@sheridancollege.ca>
2022-01-31 13:01:41 -0500

    implemented high score functionality

--- script.js
+++ script.js
@@ -1,66 +1,92 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
 const body = document.getElementById('bg-color');
 
 const checkBtn = document.querySelector('.check');
 const againBtn = document.querySelector('.again');
+const resetBtn = document.querySelector('.reset');
 
 let guessInput = document.querySelector('.guess');
 let guessMessage = document.querySelector('.message');
-let scoreLabel = document.querySelector('.label-score');
-let highScore = document.querySelector('.label-highscore');
+let scoreMsg = document.querySelector('.score');
+let highScoreMsg = document.querySelector('.highscore');
 let qmark = document.querySelector('.number');
 let score = 20;
+let highScore = 0;
+
+const displayMsg = function (message) {
+  guessMessage.textContent = message;
+};
+
+const displayScore = function (num) {
+  scoreMsg.textContent = num;
+};
+
+const displayHighScore = function (num) {
+  highScoreMsg.textContent = num;
+};
 
 checkBtn.addEventListener('click', e => {
   let newInput = Number(guessInput.value);
 
   //   no input
   if (!newInput) {
-    guessMessage.textContent = 'No number!';
+    displayMsg('No number!');
   }
   //   player wins
   else if (newInput === secretNumber) {
-    guessMessage.textContent = 'Correct';
+    displayMsg('Correct');
     body.style.backgroundColor = 'green';
     qmark.textContent = secretNumber;
     qmark.style.width = '24rem';
-    // score.textContent = Number(score.textContent) + 2;
-  }
-  //   guess is too low
-  else if (newInput < secretNumber) {
-    if (score > 1) {
-      guessMessage.textContent = 'Too low';
-      body.style.backgroundColor = 'red';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game.';
-      document.querySelector('.score').textContent = 0;
-      body.style.backgroundColor = 'red';
+    highScore = score;
+    displayHighScore(highScore);
+
+    if (score > highScore) {
+      highScore = score;
+      displayHighScore(highScore);
     }
   }
-  //   guess is too high
-  else if (newInput > secretNumber) {
+
+  // when guess is wrong
+  // use ternary operator for number too low and number too high
+  else if (newInput !== secretNumber) {
     if (score > 1) {
-      guessMessage.textContent = 'Too high';
-      body.style.backgroundColor = 'orange';
+      // guessMessage.textContent =
+      //   newInput > secretNumber ? 'Too high' : 'Too low';
+      displayMsg(newInput > secretNumber ? 'Too high' : 'Too low');
+      body.style.backgroundColor = newInput > secretNumber ? 'red' : 'orange';
       score--;
-      document.querySelector('.score').textContent = score;
+      displayScore(score);
     } else {
-      document.querySelector('.message').textContent = 'You lost the game.';
-      document.querySelector('.score').textContent = 0;
+      displayMsg('You lost the game.');
+      displayScore(0);
     }
   }
 });
 
+// play again click button even
 againBtn.addEventListener('click', e => {
+  playAgain();
+});
+
+// implement play again functionality
+function playAgain() {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
   body.style.backgroundColor = '#222';
-  guessMessage.textContent = '...Start guessing';
+  displayMsg('...Start guessing');
   guessInput.value = '';
   qmark.textContent = '?';
   qmark.style.width = '15rem';
-  document.querySelector('.score').textContent = 20;
+  displayScore(20);
   score = 20;
+}
+
+// reset the entire game
+resetBtn.addEventListener('click', e => {
+  playAgain();
+  highScore = 0;
+  displayHighScore(highScore);
 });
